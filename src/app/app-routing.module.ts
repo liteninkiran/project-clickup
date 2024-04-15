cabb51a3925f45751768ed132aec6e9844b77308
Kiran Anand <liteninkiran@hotmail.com>
2024-04-15 18:59:19 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ScratchComponent } from './components/scratch/scratch.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'scratch',
+        component: ScratchComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '/',
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
