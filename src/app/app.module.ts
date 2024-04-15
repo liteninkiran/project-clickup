97d736b6bbd7f2afd6977f7537c4f95e863ef661
Kiran Anand <liteninkiran@hotmail.com>
2024-04-15 18:53:42 +0100

    Add home/scratch/navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HomeComponent } from './components/home/home.component';
+import { ScratchComponent } from './components/scratch/scratch.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        HomeComponent,
+        ScratchComponent,
+        NavbarComponent,
     ],
     imports: [
         BrowserModule,
