85e75493dd0631e41ccfcef0f042189bb86521e7
Kiran Anand <liteninkiran@hotmail.com>
2024-04-16 20:55:18 +0100

    Remove dotenv

--- src/app/services/click-up.service.ts
+++ src/app/services/click-up.service.ts
@@ -8,7 +8,7 @@ export class ClickUpService {
     constructor(private http: HttpClient) {}
 
     public loadDataFromApi(): Observable<any> {
-        const headerBody = { 'Authorization': process.env['API_KEY'] ?? '' }
+        const headerBody = { 'Authorization': '' }
         const headers = new HttpHeaders(headerBody);
         const url = 'https://api.clickup.com/api/v2/team';
         return this.http.get(url, { headers });
